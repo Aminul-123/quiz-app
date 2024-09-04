2ba76feccd2ce198da21aafd40704d965e4d3f87
Aminul Ali <alomamin963@gmail.com>
2024-09-04 10:30:23 +0530

    added context api

--- src/App.jsx
+++ src/App.jsx
@@ -10,97 +10,12 @@ import Progress from "./assets/Progress.jsx";
 import FinishScreen from "./assets/FinishScreen.jsx";
 import Footer from "./Footer.jsx";
 import Timer from "./assets/Timer.jsx";
-
-const SECS_PER_QUESTION = 30;
-
-const initialState = {
-  questions: [],
-  // 'loading', 'error', 'ready', 'active', 'finished'
-  status: "loading",
-  index: 0,
-  answer: null,
-  points: 0,
-  highscore : 0,
-  secondsRemaining : null,
-};
-function reducer(state, action) {
-  switch (action.type) {
-    case "dataReceived":
-      return {
-        ...state,
-        questions: action.payload,
-        status: "ready",
-      };
-    case "dataFailed":
-      return {
-        ...state,
-        status: "error",
-      };
-    case "start":
-      return {
-        ...state,
-        status: "active",
-        secondsRemaining : state.questions.length * SECS_PER_QUESTION,
-      };
-    case "newAnswer":
-      const question = state.questions.at(state.index);
-      return {
-        ...state,
-        answer: action.payload,
-        points:
-          action.payload === question.correctOption
-            ? state.points + question.points
-            : state.points,
-      };
-    case 'nextQuestion' : 
-       return {
-          ...state,
-          index : state.index + 1,
-          answer : null
-    }
-    case 'finish' : 
-      return {
-        ...state,
-        status : 'finish',
-        highscore : state.points > state.highscore ? state.points : state.highscore
-      }
-      case 'restart' : 
-        return {
-          // ...state,
-          // status : 'ready',
-          // points : 0,
-          // answer : null,
-          // index : 0,
-          // highscore : 0
-          ...initialState, 
-          questions : state.questions,
-          status : 'ready'
-        }
-        case 'tick' : 
-          return {
-            ...state,
-            secondsRemaining : state.secondsRemaining - 1,
-            status : state.secondsRemaining === 0 ? 'finish' : state.status,
-          }
-    default:
-      throw new Error("Action Unknown");
-  }
-}
+import { useQuiz } from "./contexts/QuizContext.jsx";
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const { questions, status, index, answer, points , highscore, secondsRemaining} = state;
-
-  const numQuestion = questions.length;
-  const maxPossiblePoints = questions.reduce((prev, curr) => prev + curr.points, 0)
-
-  useEffect(function () {
-    fetch("http://localhost:8000/questions")
-      .then((res) => res.json())
-      .then((data) => dispatch({ type: "dataReceived", payload: data }))
-      .catch((err) => dispatch({ type: "dataFailed" }));
-  }, []);
+  const { questions, status, index, answer, points, highscore, secondsRemaining, numQuestion, maxPossiblePoints} = useQuiz()
+  
   return (
     <>
       <div className="app">
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
  import App from './App.jsx'
  import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { ContextProvider } from './contexts/QuizContext.jsx'
 // import StarRating from './assets/StarRating'
 
 // function Test () {
@@ -17,7 +18,9 @@ import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ContextProvider>
        <App />
+    </ContextProvider>
     </BrowserRouter>
     {/* <StarRating maxRating={5} messages = {['Terrible', 'Bad', 'Okay' , 'Good',  
       'Amazing'
